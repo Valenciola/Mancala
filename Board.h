b1a7ad4f7df7afe44f4fbcee042701d9213fa8bd
Valenciola <vfilsaime2021@fau.edu>
2023-11-04 02:29:56 +0000

    Created a header for the game mechanisms

---
+++ Board.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//Preprocessor Directives
+#include <iostream>
+
+//This class should run all of the game mechanisms...
+
+class Board {
+	public:
+		int StartGame(int mode); //Function to start the game
+		int Failsafe(int inprogress, int board[], int p1bank, int p2bank); //Checks if game is okay by running failsafes
+		void TakeTurn(int player, int board[]); //Iterates through a turn
+		int DecideWinner(); //Decides on a winner and works in StartGame to check if the game is over
+	private:
+		int cells[12];
+		int bank1, bank2;
+};
