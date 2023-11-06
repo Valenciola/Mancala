2ff9e9aa8bd4b98b23c31f21dd59ddbb311f6050
Valenciola <vfilsaime2021@fau.edu>
2023-11-06 17:07:19 +0000

    Created implementation file for Sir Safety

---
+++ Safety.cpp
@@ -0,0 +1,18 @@
+//Preprocessor Directives
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+//Headers
+#include "Board.h"
+#include "Safety.h"
+
+//Write SirSafety Functions
+
+bool SirSafety::NumStones(int board[], int stonecount) {
+	//Checks that there are 48 stones in the game AT ALL TIMES
+}
+bool SirSafety::LegalMove(int cell, int player) {
+	//Checks that an illegal move does not get run
+}
