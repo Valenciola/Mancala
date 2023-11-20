a56802513423f10c6a5336dd29e22a6a8c522379
Valenciola <vfilsaime2021@fau.edu>
2023-11-20 20:33:10 +0000

    Function writing but without testing because I like debugging I guess

--- Complayer.cpp
+++ Complayer.cpp
@@ -5,6 +5,55 @@ using namespace std;
 
 #include "Complayer.h"
 
-int FindBestMove(int board[], int firstbank, int secondbank) {
+int Computer::FindBestMove(int board[], int secondbank) {
 	//Finds and returns the best move
+	int move, stones, bankadd = 0, spot, code;
+	int outcomes[6], fakeboard[12];
+
+	for (int i = 0; i < 6; i++) {
+		if (board[i + 6] == 0) {
+			cout << "There's nothing in hole " << i + 1 << ". Moving on!" << endl;
+		}
+		else {
+			cout << "There's something in hole " << i + 1 << ". I'm gonna check it out." << endl;
+
+			for (int j = 0; j < 12; j++) {
+				//Board reset
+				fakeboard[i] = board[i];
+			}
+
+			stones = fakeboard[i];
+			spot = i + 6;
+			fakeboard[i] = 0;
+
+			while (stones != 0) {
+				if (spot == 12) {
+					bankadd++;
+					stones--;
+					spot = 0;
+				}
+				else if (spot == 6) {
+					spot = 7;
+				}
+				else {
+					fakeboard[spot]++;
+					stones--;
+					spot++;
+				}
+			}
+
+			outcomes[i] = bankadd;
+			move = 0;
+			for (int g = 0; g < 6; g++) {
+				if (outcomes[g] > move) {
+					move = outcomes[g];
+				}
+				else {
+					//Do nothing.
+				}
+			}
+		}
+	}
+
+	return move;
 }
